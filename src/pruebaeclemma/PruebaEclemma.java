e3e5187bedfba7405c7a6840f350d0e4c29a8c78
Typetext <typecraft@hotmail.com>
2014-02-03 20:51:59 +0100

    Yo tambien urgo codigo ajeno
    A programa regalado, no le mires el codigo

--- src/pruebaeclemma/PruebaEclemma.java
+++ src/pruebaeclemma/PruebaEclemma.java
@@ -10,7 +10,9 @@ public class PruebaEclemma {
 		int a = 12;
 		if (a >10) {
 		System.out.println("el numero "+a+" es mayor que 10");
-		System.out.println(" añado un comentario para hacer el segundo commit");
+		System.out.println(" aï¿½ado un comentario para hacer el segundo commit");
+		System.out.println("He puesto syso y le he dado a control+enter :D");
+		System.out.println("+1 por mi estupidez");
 		}
 	}
 
