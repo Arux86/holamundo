04c732491c234237b6a9d59d0231e485839de8ef
Arux86 <alb3rto86@hotmail.com>
2014-02-03 20:04:25 +0100

    primera copia

---
+++ src/pruebaeclemma/PruebaEclemma.java
@@ -0,0 +1,16 @@
+package pruebaeclemma;
+
+public class PruebaEclemma {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 12;
+		if (a >10) {
+		System.out.println("el numero "+a+" es mayor que 10");
+		}
+	}
+
+}
